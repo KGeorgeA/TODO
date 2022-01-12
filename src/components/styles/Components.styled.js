2ad3a4d0da517f50c7116d0155b4817782d05c76
KGeorgeA <karcivadzega@gmail.com>
2022-01-12 17:52:32 +0300

    added more styles

--- src/components/styles/Components.styled.js
+++ src/components/styles/Components.styled.js
@@ -15,11 +15,14 @@ export const Container = styled.main`
   background-color: ${STYLE_VARIABLES.containerBackgroundColor};
   color: ${STYLE_VARIABLES.textColor};
   line-height: ${STYLE_VARIABLES.lineHeight};
-  ${'' /* box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%), 0 25px 50px 0 rgb(0 0 0 / 10%); */}
 `
-// works not like what i wanted FIX it
+
+export const Main = styled.section`
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%), 0 25px 50px 0 rgb(0 0 0 / 10%);
+  
+`
+
 export const Heading = styled.header` 
-    background-color: ${STYLE_VARIABLES.bodyColor};
     width: 100%;
     text-align: center;
     font: 100px 'Helvetica Neue', Helvetica, Arial, sans-serif;
@@ -29,25 +32,44 @@ export const Heading = styled.header`
 
 export const Form = styled.form`
   width: 100%;
+  max-height: 60px;
+  height: 100%;
 `
 
 export const TextInput = styled.input`
   width: 100%;
   padding: 16px 16px 16px 60px;
 
+  font-size: 24px;
+
   box-sizing: border-box;
   border: none;
-  box-shadow: inset 0 -2px 1px rgb(0,0,0 / 3%);
+  ${'' /* box-shadow: inset 0 -2px 1px rgb(0,0,0 / 3%); */}
+  opacity: 30%;
+`
+
+export const TodoList = styled.ul`
+  ${'' /* box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%), 0 25px 50px 0 rgb(0 0 0 / 10%); */}
 `
 
 export const TodoItem = styled.li`
+  border-bottom: 1px solid black;
   
 `;
 
 export const TodoItemDiv = styled.div`
-  ${'' /* display: flex; */}
+  align-items: center;
   font-size: 24px;
   position: relative;
+
+    input {
+      position: absolute;
+      width: 40px;
+      height: 40px;
+      top: 0;
+      bottom: 0;
+      margin: auto 0;
+    }
 
     label {
       padding: 15px 15px 15px 60px;
@@ -56,13 +78,20 @@ export const TodoItemDiv = styled.div`
     }
     
     button {
-      ${'' /* border: none; */}
-      ${'' /* appearance: none; */}
-      position: absolute;
+      position:absolute;
+      
+      top: 0;
       right: 10px;
+      bottom: 0;
+
+      ${'' /* border: none; */}
+      background: none;
       width: 40px;
       height: 40px;
       margin: auto 0;
-      z-index: 2;
     }
 `;
+
+export const FooterStyle = styled.footer`
+  background-color: #b2b2b2;
+`
