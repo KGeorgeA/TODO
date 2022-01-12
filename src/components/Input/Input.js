71004606120dd75a7dfd5a63745ed7a2c268a2b5
KGeorgeA <karcivadzega@gmail.com>
2022-01-12 09:39:57 +0300

    deleted handleSubmit function bcs could not get Parent state

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,20 +2,10 @@ import React from "react";
 
 class Input extends React.Component {
 
-    handlerSubmit(ev) {
-        const el = document.getElementById("input");
-        let newTodosList = this.props.todos;
-        let sus = newTodosList.concat([{id: newTodosList.length + 1, value: el.value, }]);
-        console.log(sus);
-        ev.preventDefault();
-        localStorage.setItem("todos-json", JSON.stringify(sus));
-    }
-
     render() {
         return(
-            <form onSubmit={this.handlerSubmit.bind(this)}>
+            <form onSubmit={this.props.handleSubmit}>
                 <input id="input"/>
-                <input type="button" onClick={this.handlerSubmit.bind(this)}/>
             </form>
         );
     }
