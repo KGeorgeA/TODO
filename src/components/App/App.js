f3b35fc13bc74feb7531c278bd224e7ea47c675c
KGeorgeA <karcivadzega@gmail.com>
2022-01-12 09:39:24 +0300

    added handleSubmit function to add new TODOs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,15 +6,29 @@ import Todos from "../Todos/Todos";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
     this.state = {
       todos: localStorage.getItem("todos-json") ? JSON.parse(localStorage.getItem("todos-json")) : [],
     }
   }
 
+  handleSubmit(ev) {
+    ev.preventDefault();
+    const inputValue = ev.target.firstElementChild.value;
+    const newTodos = [...this.state.todos, {id: this.state.todos.length + 1, value: inputValue}];
+    console.log(newTodos);
+    localStorage.setItem("todos-json", JSON.stringify(newTodos));
+    this.setState({
+      todos: newTodos,
+    });
+    ev.target.firstElementChild.value = "";
+  }
+
   render() {
     return (
       <section>
-        <Input todos={this.state.todos}/>
+        <Input handleSubmit={this.handleSubmit}/>
         <Todos todos={this.state.todos}/>
       </section>
     );
