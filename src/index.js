2ab135e9b6a7c32b5cec80dde02b4cfa80ef99a9
KGeorgeA <karcivadzega@gmail.com>
2022-01-14 17:40:41 +0300

    added styled-components globalstyle for body

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App.js';
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #f5f5f5;
+    font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    button {
+      font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    }
+  }
+`
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle></GlobalStyle>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
